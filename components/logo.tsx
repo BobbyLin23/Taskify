3d580637182411c6df899a0db2338eb4b3a9939f
bobby lin <linzhangsheng23@gmail.com>
2023-11-14 15:55:14 +0800

    feat: add landing page navbar and footer

---
+++ components/logo.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import local from 'next/font/local'
+import { cn } from '@/lib/utils'
+
+const headingFont = local({
+  src: '../public/fonts/font.woff2',
+})
+
+export function Logo() {
+  return (
+    <Link href="/">
+      <div
+        className="hidden items-center gap-x-2 transition 
+        hover:opacity-75 md:flex"
+      >
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p
+          className={cn('pt-1 text-lg text-neutral-700', headingFont.className)}
+        >
+          Taskify
+        </p>
+      </div>
+    </Link>
+  )
+}
